575e94fc949080ff6a17293622361dad509f4b79
Carlos_PG <carlos.pedragon@alum.uca.es>
2026-05-04 17:45:20 +0200

    Añadimos un index.html minimo
    - Pasos a hacer:
    
    Rama feature/frontend-ui: Crear el index.html y conectar el botón con la API.
    
    Rama feature/static-files: Configurar FastAPI para que "sirva" este archivo HTML automáticamente.
    
    Rama feature/history-db: (El gran salto) Crear una base de datos para que la interfaz muestre "Tus últimos 5 QRs generados".

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from app.schemas.qr_schema import QRRequest
 from app.services.qr_service import generate_qr_code
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="QR Generator API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # En producción pondrías tu dominio real
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/generate-qr")
 async def create_qr(request: QRRequest):
@@ -12,7 +21,7 @@ async def create_qr(request: QRRequest):
         url_str = str(request.url)
         
         # Llamamos al servicio
-        image_buffer = generate_qr_code(url_str, request.scale)
+        image_buffer = generate_qr_code(url_str, request.scale)  # El servicio devuelve un BytesIO con la imagen del QR
         
         # Devolvemos la imagen directamente al navegador
         return StreamingResponse(image_buffer, media_type="image/png")
